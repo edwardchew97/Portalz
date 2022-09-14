d2251cb1ce359a5687a5be65cc9058e55fee5cdd
Paul Myburgh <lostpebble@protonmail.com>
2022-09-14 06:40:28 +0200

    change port again to 3000

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
-const port = 8080;
+const port = 3000;
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);

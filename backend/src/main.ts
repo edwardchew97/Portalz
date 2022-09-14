aae0d8e9e7ab2642afa580c55ad6851bef7e179d
Paul Myburgh <lostpebble@protonmail.com>
2022-09-14 06:30:35 +0200

    Changing up port to get backend running

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+const port = 8080;
+
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
 	app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-	await app.listen(3000);
+	await app.listen(port, "0.0.0.0");
 }
 bootstrap();
